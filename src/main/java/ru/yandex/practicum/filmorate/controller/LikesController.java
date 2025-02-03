127afa1307d0e1ead826aff776c71e53b4fc3316
PepeRonin <ivanpetrovskiy98@gmail.com>
2025-02-03 23:16:31 +0300

    add likes controller and enhance likes service with logging

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/LikesController.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.service.LikesService;
+
+@RestController
+@RequestMapping("/films/{id}/likes/{userId")
+@RequiredArgsConstructor
+@Slf4j
+@SuppressWarnings("unused")
+public class LikesController {
+
+    private final LikesService likesService;
+
+    @PutMapping
+    public ResponseEntity<Void> addLike(@PathVariable long id, @PathVariable long userId) {
+        log.info("Request to add like from user {} to film {} received.", userId, id);
+        likesService.likeFilm(id, userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> removeLike(@PathVariable long id, @PathVariable long userId) {
+        log.info("Request to remove like from user {} to film {} received.", userId, id);
+        likesService.unlikeFilm(id, userId);
+        return ResponseEntity.ok().build();
+    }
+
+}
