716eaac9aa11371335b92598ee351328d26b0781
PepeRonin <ivanpetrovskiy98@gmail.com>
2025-02-28 01:05:53 +0300

    simplify DbUserStorage tests by allowing linear dependencies

--- src/test/java/ru/yandex/practicum/filmorate/storage/DbUserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/DbUserStorageTest.java
@@ -1,8 +1,7 @@
-package ru.yandex.practicum.filmorate;
+package ru.yandex.practicum.filmorate.storage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +14,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.repository.DbUserStorage;
 import ru.yandex.practicum.filmorate.repository.mappers.UserRowMapper;
@@ -24,19 +22,12 @@ import ru.yandex.practicum.filmorate.repository.mappers.UserRowMapper;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({DbUserStorage.class, UserRowMapper.class})
-class FilmorateApplicationTests {
+class DbUserStorageTest {
 
     private static final String DELETE_QUERY = "DELETE FROM users";
-    private static final String ADD_USER_QUERY = "INSERT INTO users (email, login, name, "
-        + "birthday) VALUES ('test@example.com', 'testlogin', 'Test User', '2000-01-01')";
-    private static final String ADD_ANOTHER_USER_QUERY = "INSERT INTO users (email, login, name, "
-        + "birthday) VALUES ('test2@example.com', 'testlogin2', 'Test User 2', '2001-02-02')";
     private static final String CHECK_USER_QUERY =
         "SELECT EXISTS (SELECT 1 FROM users WHERE user_id = ? AND email = 'test@example.com' AND "
             + "login = 'testlogin' AND name = 'Test User' AND birthday" + " = '2000-01-01')";
-    private static final String CHECK_UPDATED_USER_QUERY =
-        "SELECT EXISTS (SELECT 1 FROM users WHERE user_id = ? AND email = 'new@example.com' AND "
-            + "login = 'newlogin' AND name = 'New User' AND birthday" + " = '1999-01-01')";
 
     private final DbUserStorage userStorage;
     private final JdbcTemplate jdbc;
@@ -49,27 +40,6 @@ class FilmorateApplicationTests {
     @AfterEach
     void tearDown() {
         jdbc.execute(DELETE_QUERY);
-    }
-
-    @Test
-    void testGetUserById() {
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbc.update(connection -> connection.prepareStatement(ADD_USER_QUERY,
-            Statement.RETURN_GENERATED_KEYS), keyHolder);
-
-        Long assignedId = keyHolder.getKeyAs(Long.class);
-
-        // Retrieve the test user that we added
-        Optional<User> userOptional = userStorage.getUserById(assignedId);
-
-        assertThat(userOptional).isPresent().hasValueSatisfying(retrievedUser -> {
-            assertThat(retrievedUser).hasFieldOrPropertyWithValue("id", assignedId);
-            assertThat(retrievedUser).hasFieldOrPropertyWithValue("email", "test@example.com");
-            assertThat(retrievedUser).hasFieldOrPropertyWithValue("login", "testlogin");
-            assertThat(retrievedUser).hasFieldOrPropertyWithValue("name", "Test User");
-            assertThat(retrievedUser).hasFieldOrPropertyWithValue("birthday",
-                LocalDate.of(2000, 1, 1));
-        });
     }
 
     @Test
@@ -87,45 +57,88 @@ class FilmorateApplicationTests {
     }
 
     @Test
+    void testGetUserById() {
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setLogin("testlogin");
+        user.setName("Test User");
+        user.setBirthday(LocalDate.of(2000, 1, 1));
+        long userId = userStorage.addUser(user);
+
+        Optional<User> userOptional = userStorage.getUserById(userId);
+
+        assertThat(userOptional).isPresent().hasValueSatisfying(retrievedUser -> {
+            assertThat(retrievedUser).hasFieldOrPropertyWithValue("id", userId);
+            assertThat(retrievedUser).hasFieldOrPropertyWithValue("email", "test@example.com");
+            assertThat(retrievedUser).hasFieldOrPropertyWithValue("login", "testlogin");
+            assertThat(retrievedUser).hasFieldOrPropertyWithValue("name", "Test User");
+            assertThat(retrievedUser).hasFieldOrPropertyWithValue("birthday",
+                LocalDate.of(2000, 1, 1));
+        });
+    }
+
+    @Test
     void testUpdateUser() {
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbc.update(connection -> connection.prepareStatement(ADD_USER_QUERY,
-            Statement.RETURN_GENERATED_KEYS), keyHolder);
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setLogin("testlogin");
+        user.setName("Test User");
+        user.setBirthday(LocalDate.of(2000, 1, 1));
+        long userId = userStorage.addUser(user);
 
-        Long assignedId = keyHolder.getKeyAs(Long.class);
-
-        // Update the test user
         User updatedUser = new User();
-        updatedUser.setId(assignedId);
+        updatedUser.setId(userId);
         updatedUser.setEmail("new@example.com");
         updatedUser.setLogin("newlogin");
         updatedUser.setName("New User");
         updatedUser.setBirthday(LocalDate.of(1999, 1, 1));
         userStorage.updateUser(updatedUser);
 
-        assertThat(Boolean.TRUE.equals(
-            jdbc.queryForObject(CHECK_UPDATED_USER_QUERY, Boolean.class, assignedId))).isTrue();
+        assertThat(userStorage.getUserById(userId)).isPresent()
+            .hasValueSatisfying(retrievedUser -> {
+                assertThat(retrievedUser).hasFieldOrPropertyWithValue("id", userId);
+                assertThat(retrievedUser).hasFieldOrPropertyWithValue("email", "new@example.com");
+                assertThat(retrievedUser).hasFieldOrPropertyWithValue("login", "newlogin");
+                assertThat(retrievedUser).hasFieldOrPropertyWithValue("name", "New User");
+                assertThat(retrievedUser).hasFieldOrPropertyWithValue("birthday",
+                    LocalDate.of(1999, 1, 1));
+            });
     }
 
     @Test
     void testGetAllUsers() {
-        jdbc.execute(ADD_USER_QUERY);
-        jdbc.execute(ADD_ANOTHER_USER_QUERY);
+        User user1 = new User();
+        user1.setEmail("test1@example.com");
+        user1.setLogin("testlogin1");
+        user1.setName("Test User 1");
+        user1.setBirthday(LocalDate.of(2001, 1, 1));
+        long user1Id = userStorage.addUser(user1);
+        user1.setId(user1Id);
+
+        User user2 = new User();
+        user2.setEmail("test2@example.com");
+        user2.setLogin("testlogin2");
+        user2.setName("Test User 2");
+        user2.setBirthday(LocalDate.of(2002, 2, 2));
+        long user2Id = userStorage.addUser(user2);
+        user2.setId(user2Id);
 
         List<User> users = (List<User>) userStorage.getAllUsers();
         assertThat(users.size()).isEqualTo(2);
+        assertThat(users).contains(user1, user2);
     }
 
     @Test
     void testCheckUserExists() {
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbc.update(connection -> connection.prepareStatement(ADD_USER_QUERY,
-            Statement.RETURN_GENERATED_KEYS), keyHolder);
+        User user = new User();
+        user.setEmail("test@example.com");
+        user.setLogin("testlogin");
+        user.setName("Test User");
+        user.setBirthday(LocalDate.of(2000, 1, 1));
+        long userId = userStorage.addUser(user);
 
-        Long assignedId = keyHolder.getKeyAs(Long.class);
-
-        assertThat(userStorage.checkUserExists(assignedId)).isTrue();
-        assertThat(userStorage.checkUserExists(assignedId + 42L)).isFalse();
+        assertThat(userStorage.checkUserExists(userId)).isTrue();
+        assertThat(userStorage.checkUserExists(userId + 42L)).isFalse();
     }
 
 }
